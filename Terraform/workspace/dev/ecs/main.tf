6f177d3ea67595ca9ac7d8549e026b5fa2b110cf
Vilozni Daniel <viloznidaniel@gmail.com>
2024-10-01 22:31:38 +0300

    update

--- Terraform/workspace/dev/ecs/main.tf
+++ Terraform/workspace/dev/ecs/main.tf
@@ -1,12 +1,28 @@
-module "ecs" {
+# First ECS Service for Microservice 1
+module "ecs_microservice1" {
   source              = "../../../modules/ecs"
   cluster_name        = "my-ecs-cluster"
-  family              = "my-microservice"
-  container_definitions = file("container_definitions.json")  # Path to the container_definitions
+  family              = "microservice1"  # Task definition family name for microservice 1
+  container_definitions = file("container_definitions_microservice1.json")  # Path to the container definitions for microservice 1
   memory              = 512
   cpu                 = 256
-  execution_role_arn  = aws_iam_role.ecs_task_execution_role.arn  # IAM role for the task execution
-  service_name        = "my-service"
+  execution_role_arn  = aws_iam_role.ecs_task_execution_role.arn
+  service_name        = "service1"
+  desired_count       = 1
+  subnet_ids          = module.vpc.public_subnet_ids
+  security_groups     = [module.vpc.ecs_security_group_id]
+}
+
+# Second ECS Service for Microservice 2
+module "ecs_microservice2" {
+  source              = "../../../modules/ecs"
+  cluster_name        = "my-ecs-cluster"
+  family              = "microservice2"  # Task definition family name for microservice 2
+  container_definitions = file("container_definitions_microservice2.json")  # Path to the container definitions for microservice 2
+  memory              = 512
+  cpu                 = 256
+  execution_role_arn  = aws_iam_role.ecs_task_execution_role.arn
+  service_name        = "service2"
   desired_count       = 1
   subnet_ids          = module.vpc.public_subnet_ids
   security_groups     = [module.vpc.ecs_security_group_id]
@@ -34,3 +50,4 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
